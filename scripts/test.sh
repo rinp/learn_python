7de258346a4819a3a800fd188dba32af6287bd28
rinp <program.javac@gmail.com>
2026-01-10 17:07:53 +0900

    docker commandに変更

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,9 +7,9 @@ COMPOSE_PROJECT_NAME="$(basename "$PWD")_test"
 
 cleanup() {
   # `-v` を付けると uv_tools_venv / uv_tools_cache も消える
-  docker-compose -f docker-compose-test.yml down -v
+  docker compose -f docker-compose-test.yml down -v
 }
 trap cleanup EXIT
 
-docker-compose -f docker-compose-test.yml up --build -d
-docker-compose -f docker-compose-test.yml exec test_web uv run pytest --cov=app
+docker compose -f docker-compose-test.yml up --build -d
+docker compose -f docker-compose-test.yml exec test_web uv run pytest --cov=app
